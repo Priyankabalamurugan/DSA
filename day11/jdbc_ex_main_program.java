169ced9b3705c1a71119fb6c7666516962a2d72a
Priyankabalamurugan <priyankabalamurugan02@gmail.com>
2025-06-19 10:19:27 +0530

    Update jdbc_ex_main_program.java

--- day11/jdbc_ex_main_program.java
+++ day11/jdbc_ex_main_program.java
@@ -1,5 +1,26 @@
 package jdbc_ex;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class jdbc_ex_main_program {
+	
 
-}
+		public static void delete(Statement stmt) throws SQLException {
+			String query="DELETE FROM student WHERE mark=70 ";
+			stmt.executeUpdate(query);
+			System.out.println("Query executed");
+		}
+
+		public static void main(String[] args) throws ClassNotFoundException, SQLException {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url="jdbc:mysql://localhost:3306/sample";
+			String username="root";
+			String password="";
+			Connection con=DriverManager.getConnection(url,username,password);
+			Statement stmt=con.createStatement();
+			delete(stmt);
+			
+		}
+	}
